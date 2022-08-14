e61aaa23b00839d32ec8ce2ab40371e3720303c8
PatriikPlays <61064909+PatriikPlays@users.noreply.github.com>
2022-08-14 15:25:56 +0200

    fixed error in achievements.java

--- src/main/java/mods/eln/Achievements.java
+++ src/main/java/mods/eln/Achievements.java
@@ -20,7 +20,7 @@ public class Achievements {
         craft50VMacerator = new Achievement(TR("achievement.craft_50v_macerator"),
             "craft_50v_macerator", 0, 2, Eln.findItemStack("50V Macerator", 0), openGuide);
 
-        TR_DESC(Type.ACHIEVEMENT, "craft_50v_macerator");*/
+        TR_DESC(Type.ACHIEVEMENT, "craft_50v_macerator");
 
         craft50VSocket = new Achievement(TR("achievement.craft_50v_socket"),
             "craft_50v_socket", 2, 2, Eln.findItemStack("50V Power Socket", 0), craft50VMacerator);
